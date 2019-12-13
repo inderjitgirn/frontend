7a3df351ef3fe9df4d0af1fdd8957962df066cd2
inderjitgirn <inderjit.yapits@gmail.com>
2019-12-13 11:59:49 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          bbbbbbbbbbbbbbbbbbbbbbbbbbs
+          learn react
         </a>
       </header>
     </div>
